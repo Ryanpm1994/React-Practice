98fe4d108fa5454cf149728cb81b4dc413074de7
Ryanpm1994 <48578847+Ryanpm1994@users.noreply.github.com>
2019-10-23 16:10:24 +0100

    Add files via upload

---
+++ ParentChildComps/src/comps/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Nav from "./Nav"
+import MainCont from "./MainCont"
+import Footer from "./Footer"
+
+
+function App() {
+  return (
+    <div>
+      <Nav/>
+      <MainCont/>
+      <Footer/>
+    </div>
+  )
+}
+
+  export default App
---
+++ ParentChildComps/src/comps/MainCont.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function MainCont() {
+    return(
+        <div>
+            <p>This is main content area...</p>
+        </div>
+    )
+}
+
+export default MainCont 
